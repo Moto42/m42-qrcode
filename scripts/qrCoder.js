787549ec185610752efdc27d52e312a6056bde9b
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-09-25 01:32:38 -0500

    awesome color changing action

--- scripts/qrCoder.js
+++ scripts/qrCoder.js
@@ -13,31 +13,39 @@ function qrcodeToMatrix(code) {
     return output;
 }
 
+function getRect(x,y,height,width,isdark=false,hue=160){
+    color = `hsl(${hue},${Math.random()*60+40}%,${Math.random()*30}%)`;
+    const rect = d3.create('svg:rect')
+        .attr('x',x)
+        .attr('y',y)
+        .attr('height',height)
+        .attr('width', width);
+    if(isdark) rect.attr('fill', color);
+    else rect.attr('fill', 'white');
+    return rect;
+}
+
 function makenewcode(event){
     const text = $('.qrCoder__text')[0].value;
-    const code = qrcode(0,'M')
+    const code = qrcode(0,'H')
     code.addData(text);
     code.make();
     const moduleCount = code.getModuleCount();
     const moduleSize = 5;
 
     $('.qrCoder__outputs').children().remove()
-    
+
     const svg = d3.select('.qrCoder__outputs').append("svg")
         .attr('height',moduleCount*moduleSize)
         .attr('width',moduleCount*moduleSize);
     
     const codeMatrix = qrcodeToMatrix(code);
+    const hue = Math.random()*360;
     for(let rowNum in codeMatrix){
         const row = codeMatrix[rowNum];
         for(let moduleNum in row){
-            const rect = svg.append('rect')
-                .attr('x',moduleNum*moduleSize)
-                .attr('y',rowNum*moduleSize)
-                .attr('height', moduleSize)
-                .attr('width', moduleSize);
-            if(codeMatrix[rowNum][moduleNum]) rect.attr('fill', 'black');
-            else rect.attr('fill', 'white');
+            const rect = getRect(moduleSize*moduleNum, moduleSize*rowNum,moduleSize, moduleSize, codeMatrix[rowNum][moduleNum],hue);
+            svg.append(()=>rect.node());
         }
     }
 }
