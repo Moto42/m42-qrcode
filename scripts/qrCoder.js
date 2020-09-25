23e6f7e531ace9e00e4044fa8f816262bd11880b
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-09-25 00:46:05 -0500

    qrcodeToMatrix is working

--- scripts/qrCoder.js
+++ scripts/qrCoder.js
@@ -2,6 +2,15 @@
 
 function qrcodeToMatrix(code) {
     const width = code.getModuleCount();
+    const output = [];
+    for(let i = 0; i < width; i++){ output.push([]) }
+
+    for(let y = 0; y < width; y++){
+        for(let x = 0; x < width; x++){
+            output[y][x] = code.isDark(y,x);
+        }
+    }
+    return output;
 }
 
 function makenewcode(event){
@@ -9,10 +18,32 @@ function makenewcode(event){
     const code = qrcode(0,'M')
     code.addData(text);
     code.make();
-    const tag = code.createImgTag(2);
-    const output = $('.qrCoder__outputs')
-    output.children().remove()
-    output.append(tag);
+    const moduleCount = code.getModuleCount();
+    const moduleSize = 5;
+
+    $('.qrCoder__outputs').children().remove()
+    $('.qrCoder__outputs')[0].innerHTML = code.createImgTag(moduleSize);
+
+    const svg = d3.select('.qrCoder__outputs').append("svg")
+        .attr('height',moduleCount*moduleSize)
+        .attr('width',moduleCount*moduleSize);
+    
+    const codeMatrix = qrcodeToMatrix(code);
+    for(let rowNum in codeMatrix){
+        const row = codeMatrix[rowNum];
+        for(let moduleNum in row){
+            const rect = svg.append('rect')
+                .attr('x',moduleNum*moduleSize)
+                .attr('y',rowNum*moduleSize)
+                .attr('height', moduleSize)
+                .attr('width', moduleSize);
+            if(codeMatrix[rowNum][moduleNum]) rect.attr('fill', 'black');
+            else rect.attr('fill', 'white');
+        }
+    }
 }
 
 $('.qrCoder__submit')[0].addEventListener('click',makenewcode);
+const code = qrcode(0,'M')
+code.addData('plu');
+code.make();
